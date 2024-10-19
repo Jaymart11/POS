4ba5fe4e608f2dd6b722651b64a38aca13d761c9
Jaymart11 <jaymarttandoc1@gmail.com>
2024-10-19 20:59:01 +0800

    make db pool

--- admin/database.js
+++ admin/database.js
@@ -3,15 +3,20 @@
 const mysql = require("mysql2");
 const config = require("./config");
 
-const db = mysql.createConnection(config.database);
+const db = mysql.createPool({
+  ...config.database,
+  waitForConnections: true, // ensures connections are queued when pool is full
+  connectionLimit: 10, // maximum number of connections in the pool
+  queueLimit: 0, // unlimited queueing of connection requests
+});
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) {
     console.error("Error connecting to database: " + err.stack);
-    db.destroy();
     return;
   }
-  console.log("Connected to database as id " + db.threadId);
+  console.log("Connected to database as id " + connection.threadId);
+  connection.release(); // release the connection back to the pool
 });
 
 module.exports = db;
